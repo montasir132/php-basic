82b3cb0d52ebfdd0a2d531ec859d1feec18483d6
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2024-12-07 19:52:23 +0600

    Add files via upload

---
+++ html.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!--required meta tags-->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="index, follow">
+        <meta name="language" content="English">
+        <meta name="revisit-after" content="3 days">
+        <meta name="author" content="Montasir alam">
+        <title>PHPbasic</title>
+    </head>
+    <body>
+        <h1></h1>
+        <!-- php start -->
+        <?php
+        echo "<h1>"."echo start..."."</h1>";
+        // php echo start
+        echo "<h2>"."Hello World".""."How are you?"."</h2>";
+
+        $name="montasir";
+        $age= 17;
+        echo "<h1>"."Hi I am a ".""."$name"."</h1>";
+        echo "<h1>"."MY Age".""."$age".""." year old"."</h1>";
+        /* php echo end
+        php print start*/
+        print "<h1>"."php print start..."."</h1>"."";
+
+        print "<p>"."Hello World"."</p>";
+        ?>
+        <!--js links-->
+        <script src="js/jquery-3.7.1.min.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/script.js"></script>
+    </body>
+</html>
